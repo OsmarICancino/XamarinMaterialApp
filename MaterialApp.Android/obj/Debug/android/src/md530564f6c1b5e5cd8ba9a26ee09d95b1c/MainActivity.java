ffdd065edf8a27f92c21e959434d2710a6a396d8
Osmar Ismael Cancino Díaz <osmaricancino@MacBook-Air-de-Osmar.local>
2019-03-06 23:31:51 -0600

    Initial commmit

---
+++ MaterialApp.Android/obj/Debug/android/src/md530564f6c1b5e5cd8ba9a26ee09d95b1c/MainActivity.java
@@ -0,0 +1,47 @@
+package md530564f6c1b5e5cd8ba9a26ee09d95b1c;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("MaterialApp.Droid.MainActivity, MaterialApp.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MaterialApp.Droid.MainActivity, MaterialApp.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
